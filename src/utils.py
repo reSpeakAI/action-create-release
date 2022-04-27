6d13e64b3a689d53bc9028a4a4dc5d06eb89cdb1
Ryan Knowles <Ryan-Knowles@users.noreply.github.com>
2022-04-27 07:07:20 -0400

    Updated previous release sorting to sort by semver instead of creation date

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ from typing import List
 from github import Repository, GitRelease
 from github.WorkflowRun import WorkflowRun
 import re
+from functools import cmp_to_key
 
 
 def get_running_workflows(repo: Repository,
@@ -9,7 +10,6 @@ def get_running_workflows(repo: Repository,
                           workflow_ids: List[int] = [],
                           branches: List[str] = []
                           ) -> List[WorkflowRun]:
-
     running_workflows = []
 
     # Filter by status
@@ -33,16 +33,55 @@ def get_running_workflows(repo: Repository,
 def get_previous_version(repo: Repository) -> str | None:
     """Return the most recent version tag found"""
     # Look at releases to find the previous version
-    releases = list(repo.get_releases())
+    releases = [r.tag_name for r in list(repo.get_releases())]
+
+    def sort_semver(r1, r2):
+        """
+        Sorts in descending semver order, so highet version first.
+        Example Ordering:
+            v2.0.0
+            v1.9.10-build.20
+            v1.9.10-build.19
+            v1.9.10-build.0
+            v1.9.10
+            v1.9.9-build.4
+            v1.9.9
+        Return negative value for r1 before r2, postive for r1 after r2, 0 for equal
+        Example Input/Output:
+            r1=v1.0.0, r2=v2.0.0, result should return positive
+            r1=v2.0.0-build.0, r2=v2.0.0, result should return negative
+            r1=v2.0.0-build.0, r2=v2.0.1, result should return positive
+        """
+
+        # Official semantic version regex from https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+        reg = re.compile(
+            r'^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$')
+        # Strip out the v in the version number and match it to the regex
+        match1 = reg.match(r1.replace('v', ''))
+        match2 = reg.match(r2.replace('v', ''))
+
+        cmp_major = int(match2['major']) - int(match1['major'])
+        if cmp_major != 0:
+            return cmp_major
+
+        cmp_minor = int(match2['minor']) - int(match1['minor'])
+        if cmp_minor != 0:
+            return cmp_minor
+
+        cmp_patch = int(match2['patch']) - int(match1['patch'])
+        if cmp_patch != 0:
+            return cmp_patch
+
+        cmp_build = int((match2['prerelease'] or '.-1').split('.')[1]) - int((match1['prerelease'] or '.-1').split('.')[1])
+        if cmp_build == 0:
+            print(f'Warning: found equal release versions:\n\tr1:{r1}\n\tr2:{r2}')
+
+        return cmp_build
 
     # Sort releases by created date, most recent first
-    releases.sort(reverse=True, key=lambda x: x.created_at)
+    releases.sort(key=cmp_to_key(sort_semver))
 
-    # Get the latest release
-    if releases:
-        return releases[0].tag_name
-
-    print('Can\'t find release')
+    return releases[0] if releases else None
 
 
 def bump_version(previous_version: str, bump_type: str, build_name: str) -> str:
